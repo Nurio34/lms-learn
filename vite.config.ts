5e16250402f8672dab19122338b0559e542fa8b4
Nurio34 <nurioonsoftware@gmail.com>
2024-10-30 02:38:25 +0300

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "My LMS Learn App",
+                short_name: "PWA",
+                description: "Bring instructors and students together",
+                theme_color: "#ffffff",
+                background_color: "#ffffff",
+                display: "standalone",
+                orientation: "portrait",
+                start_url: "/",
+                scope: "/",
+                icons: [
+                    {
+                        src: "icons/icon-192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "icons/icon-512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+            workbox: {
+                // Optional Workbox configurations
+                globPatterns: ["**/*.{js,css,html,png,jpg,jpeg,svg}"],
+            },
+        }),
+    ],
+});
